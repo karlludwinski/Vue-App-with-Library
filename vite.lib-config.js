69add8cc006b43ff0f00228cc0a32dfb8841e26f
Karl Ludwinski <karl.ludwinski@altramotion.com>
2024-11-27 09:26:05 -0500

    Export HelloWorld.vue as a library package

---
+++ vite.lib-config.js
@@ -0,0 +1,39 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/library.js'),
+      name: 'MyLib',
+      // the proper extensions will be added
+      fileName: 'my-lib',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+})
